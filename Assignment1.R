0951e7284df29be152e9225751bb7ec9c1db46da
Shelmith-Kariuki <kariukishelmith@gmail.com>
2020-03-05 14:01:02 +0300

    Showing how to add a file onto a new github repository

---
+++ Assignment1.R
@@ -0,0 +1,33 @@
+## Name: Amandla
+## Assignment: 1
+## Date: 29th February, 2020
+
+## You are provided with the Financial Inclusion in Africa dataset, 
+## which is hosted on the Zindi platform. https://zindi.africa/competitions/financial-inclusion-in-africa.
+## Use the dataset to answer the questions below.
+
+## NB: Please make your work neat by including spaces in the code e.g
+## my_df <- df %>% 
+##          filter(Age == 35)
+
+##1. At the beginning of the training, we learnt how to tell a story from data.
+## List down 5 or more questions that we can ask from the dataset.
+
+##2. Generate a dataset that only contains observations from Kenya. We will 
+## use this dataset to answer subsequent questions. Call the dataset Kenya_df.
+
+##3. Drop the "unique_id" and "year" variables from the dataset.
+
+##4. Generate a variable called "household_cat" that re-categorizes the "household
+## size" variable into 5 groups. Group 1 (1-5), Group 2 (6-10), Group 3 (11-15),
+## Group 4 (>15). 
+
+##5. Generate a variable called "age_cat" that categorizes the "age" variable into three buckets.
+## Use limits of your own choice.
+
+##6. Generate a variable called "relationship_with_head2", that combines the 
+## "other relative" and "Other non-relatives" options of the "relationship_with_head"
+## variable into a single option called "other".
+
+##7. Generate a dataset that contains self employed ladies who have never been married,
+## they have no cellphone access, but they have bank accounts. Call this dataset "empowered".
